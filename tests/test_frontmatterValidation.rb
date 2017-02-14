ca9b13531cddba399f696c50bcaf4db209f18c02
Steve Winton <stevewinton@gmail.com>
2017-02-14 11:16:17 -0600

    Add message to YAML front matter assertions
    To ease debugging, per: https://ruby-doc.org/stdlib-2.1.1/libdoc/test/unit/rdoc/Test/Unit/Assertions.html#method-i-assert

--- tests/test_frontmatterValidation.rb
+++ tests/test_frontmatterValidation.rb
@@ -8,13 +8,13 @@ class TestFrontMatterValidation < Test::Unit::TestCase
             data = YAML.load_file(path)
 
             keys = data.keys.map { |k| k.downcase }
-            assert(keys.include? "githubhandle")
-            assert(keys.include? "latitude")
-            assert(keys.include? "longitude")
+            assert(keys.include? "githubhandle", "githubhandle missing: #{path}")
+            assert(keys.include? "latitude", "latitude missing: #{path}")
+            assert(keys.include? "longitude", "longitude missing: #{path}")
 
             lines = File.readlines(path)
-            assert_equal("---\n", lines[0])
-            assert(lines[4..-1].include? "---\n")
+            assert_equal("---\n", lines[0], "opening --- missing: #{path}")
+            assert(lines[4..-1].include? "---\n", "closing --- missing: #{path}")
         end
     end
 
