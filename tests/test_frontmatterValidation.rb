548cdb62b7af6a1155b72e8cc12317d7481b2a55
Stefan Stölzle <stoe@github.com>
2017-02-14 14:50:30 +0100

    Use assert not assert_true

--- tests/test_frontmatterValidation.rb
+++ tests/test_frontmatterValidation.rb
@@ -8,13 +8,13 @@ class TestFrontMatterValidation < Test::Unit::TestCase
             data = YAML.load_file(path)
 
             keys = data.keys.map { |k| k.downcase }
-            assert_true(keys.include? "githubhandle")
-            assert_true(keys.include? "latitude")
-            assert_true(keys.include? "longitude")
+            assert(keys.include? "githubhandle")
+            assert(keys.include? "latitude")
+            assert(keys.include? "longitude")
 
             lines = File.readlines(path)
             assert_equal("---\n", lines[0])
-            assert_true(lines[4..-1].include? "---\n")
+            assert(lines[4..-1].include? "---\n")
         end
     end
 
